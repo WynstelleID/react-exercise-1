b9767227720a95c5d598ce15bbc3710a7c6e6b12
Dandi Setiyawan <dandisetiyawan@Dandis-MacBook-Pro.local>
2023-07-02 23:43:54 +0700

    learn react

---
+++ src/components/Concepts/ConceptItem.js
@@ -0,0 +1,15 @@
+const ConceptItem = (props) => {
+  return (
+    <div>
+      <ul id="concepts">
+        <li className="concept">
+          <img src={props.image} alt={props.title} />
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ConceptItem;
